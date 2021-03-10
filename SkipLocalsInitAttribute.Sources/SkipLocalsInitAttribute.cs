a812e673f8b66150fe8353765771b06dda799af1
Fabrício Godoy <skarllot@gmail.com>
2021-03-10 10:11:38 -0300

    Create SkipLocalsInitAttribute

---
+++ SkipLocalsInitAttribute.Sources/SkipLocalsInitAttribute.cs
@@ -0,0 +1,21 @@
+#if !NET5_0
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(
+        AttributeTargets.Module |
+        AttributeTargets.Class |
+        AttributeTargets.Struct |
+        AttributeTargets.Interface |
+        AttributeTargets.Constructor |
+        AttributeTargets.Method |
+        AttributeTargets.Property |
+        AttributeTargets.Event,
+        Inherited = false)]
+    internal sealed class SkipLocalsInitAttribute : Attribute
+    {
+        public SkipLocalsInitAttribute()
+        {
+        }
+    }
+}
+#endif
